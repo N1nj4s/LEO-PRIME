0a2da030ef1ba34396d69e737bb2ce1f94ed352e
N1nj4s <leonardozola04@gmail.com>
2025-04-10 20:50:22 -0300

    realizado

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-// Exibir loading
-function toggleLoading() {
-    let loading = document.querySelector(".loader");
-    loader.style.dysplay = loader.style.dysplay == "nome" ? "block" : "nome";    
+//API key
+const options = {
+    method: 'GET', 
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDljYTA5YTNjMmMyNTY3ZjMzNmU3OGQ2MjI5MjQwMyIsIm5iZiI6MTc0MjUxMjI1MS43ODMsInN1YiI6IjY3ZGNhMDdiMzUwODkxMTQ1MjZiYTlkNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.MPm0dGLGnh4q7e2cfwLMFhk04BzswCTs4-DuJqDFbo8'
+    }
 }
 
-// Page scoll
-window.addEventListener("scroll", function () {
-    const navbar = this.document.querySelector(".navbar");
+// Exibir loading 
+function toggleLoading() { 
+    let loader = document.querySelector(".loader"); 
+    loader.style.display = loader.style.display === "none" ? "block" : "none"; 
+} 
 
-    if (this.window.scrollY > 50) {
-        navbar.classList.add("scrolled");
-    } else {
-        navbar.classList.remove("scrolled")
-    }
+// Page Scroll 
+window.addEventListener("scroll", function () { 
+    const navbar = document.querySelector(".navbar"); 
+    if (window.scrollY > 50) {  // Corrigido "scrolly" para "scrollY"
+        navbar.classList.add("scrolled"); 
+    } else { 
+        navbar.classList.remove("scrolled"); 
+    } 
 });
